1e5be42f1df66c00ba3adba4d5dd6d4af65b1c98
Herlyn David <herlindavid269@gmail.com>
2023-06-13 09:57:20 -0500

    Validaciones de usuarios, roles y login

---
+++ js/funcionalidadesLogin/validacionLogin.js
@@ -0,0 +1,128 @@
+(() => {
+    const formulario = document.querySelector('#formularioLogin');
+
+
+    const submit = document.querySelector(
+        '#formularioLogin input[type="submit"]'
+    );
+
+    const atras = document.querySelector('#xAgregar');
+
+    window.addEventListener('load', () => {
+        submit.addEventListener('click', crearRol);
+        cancelar.addEventListener('click', recetearFormulario);
+        atras.addEventListener('click', recetearFormulario);
+    });
+
+    function recetearFormulario(e){
+        e.preventDefault();
+        formulario.reset();
+    }
+
+    function crearRol(e) {
+        e.preventDefault();
+
+        /// Validar el formulario
+        validarRol();
+    }
+
+    function validarRol() {
+        //* Campos a validar
+
+        const usuario = document.querySelector(
+            '#formularioLogin input[name="usuarioGuardar"]'
+        );
+
+        const contrasena = document.querySelector(
+            '#formularioLogin input[name="contraseñaGuardar"]'
+        );
+
+        //- Expresiones Regulares
+        const number = /^\D*$/;
+        const text = /^[^a-zA-Z]*$/;
+        const email_val =
+            /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        //* Contenedores del formularios
+        const divUsuario = document.querySelector(
+            '#formularioLogin div[name="divUsuario"]'
+        );
+
+        const divContraseña = document.querySelector(
+            '#formularioLogin div[name="divContraseña"]'
+        );
+        
+        /// Lógica de validación
+
+        let isValidado = true;
+
+        //* Validaciones para el nombre
+        if (usuario.value == '') {
+            imprimirAlerta('El usuario es obligatorio', divUsuario, 'Usuario');
+            isValidado = false;
+        }
+
+        if (contrasena.value == '') {
+            imprimirAlerta('La contraseña es obligatoria', divUsuario, 'Usuario');
+            isValidado = false;
+        }
+
+        if (isValidado) {
+            //* Serrando el modal
+            const modalBootstrap = bootstrap.Modal.getInstance(
+                document.querySelector('#myModal')
+            );
+            modalBootstrap.hide();
+
+            formulario.reset();
+
+            mostrarToast('Rol agregado correctamente');
+        }
+    }
+
+    function imprimirAlerta(mensaje, lugar, clase) {
+        /// Verificar que no exista la alerta
+        const alert = document.querySelector(`.alerta${clase}`);
+
+        if (!alert) {
+            //? Crear alerta
+            const divMensaje = document.createElement('div');
+
+            divMensaje.classList.add(
+                // 'px-2',
+                'py-1',
+                'rounded',
+                'max-w-lg',
+                'mx-auto',
+                'mt-2',
+                'text-center',
+                'border',
+                `alerta${clase}`
+            );
+
+            divMensaje.classList.add(
+                'bg-red-100',
+                'border-red-400',
+                'text-red-700'
+            );
+
+            divMensaje.textContent = mensaje;
+
+            lugar.parentNode.insertBefore(divMensaje, lugar.nextSibling);
+
+            setTimeout(() => {
+                divMensaje.remove();
+            }, 4500);
+        }
+    }
+
+    function mostrarToast(mensaje) {
+        const toastDiv = document.querySelector('#toastAgregar'); //* Seleccionamos el toast que esta en nuestro HTML
+        const toastBody = document.querySelector('#toast-body-agregar'); //* Y también el body para agregar contenido a nuestro toast
+        /// Creamos la instancia
+        const toast = new bootstrap.Toast(toastDiv);
+        toastBody.textContent = mensaje;
+        /// Mostrando el mensaje
+        toast.show();
+    }
+})();
---
+++ js/funcionalidadesRoles/validacionRolEditar.js
@@ -0,0 +1,120 @@
+(() => {
+    const formulario = document.querySelector('#formularioeditarrol');
+
+
+    const submit = document.querySelector(
+        '#formularioeditarrol input[type="submit"]'
+    );
+
+    const cancelar = document.querySelector('#editarCancelado');
+    const atras = document.querySelector('#xEditar');
+
+    window.addEventListener('load', () => {
+        submit.addEventListener('click', crearRol);
+        cancelar.addEventListener('click', recetearFormulario);
+        atras.addEventListener('click', recetearFormulario);
+    });
+
+    function recetearFormulario(e){
+        e.preventDefault();
+        formulario.reset();
+    }
+
+    function crearRol(e) {
+        e.preventDefault();
+
+        /// Validar el formulario
+        validarRol();
+    }
+
+    function validarRol() {
+        //* Campos a validar
+
+        const nombre = document.querySelector(
+            '#formularioeditarrol input[name="nombreGuardar"]'
+        );
+
+        //- Expresiones Regulares
+        const number = /^\D*$/;
+        const text = /^[^a-zA-Z]*$/;
+        const email_val =
+            /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        //* Contenedores del formularios
+        const divNombre = document.querySelector(
+            '#formularioeditarrol div[name="divNombre"]'
+        );
+        
+        /// Lógica de validación
+
+        let isValidado = true;
+
+        //* Validaciones para el nombre
+        if (nombre.value == '') {
+            imprimirAlerta('El nombre es obligatorio', divNombre, 'Nombre');
+            isValidado = false;
+        } else if (!number.test(nombre.value)) {
+            imprimirAlerta('El nombre no puede contener números', divNombre),
+                'Nombre';
+            isValidado = false;
+        }
+
+        if (isValidado) {
+            //* Serrando el modal
+            const modalBootstrap = bootstrap.Modal.getInstance(
+                document.querySelector('#modalEditar')
+            );
+            modalBootstrap.hide();
+
+            formulario.reset();
+
+            mostrarToast('Rol editado correctamente');
+        }
+    }
+
+    function imprimirAlerta(mensaje, lugar, clase) {
+        /// Verificar que no exista la alerta
+        const alert = document.querySelector(`.alerta${clase}`);
+
+        if (!alert) {
+            //? Crear alerta
+            const divMensaje = document.createElement('div');
+
+            divMensaje.classList.add(
+                // 'px-2',
+                'py-1',
+                'rounded',
+                'max-w-lg',
+                'mx-auto',
+                'mt-2',
+                'text-center',
+                'border',
+                `alerta${clase}`
+            );
+
+            divMensaje.classList.add(
+                'bg-red-100',
+                'border-red-400',
+                'text-red-700'
+            );
+
+            divMensaje.textContent = mensaje;
+
+            lugar.parentNode.insertBefore(divMensaje, lugar.nextSibling);
+
+            setTimeout(() => {
+                divMensaje.remove();
+            }, 4500);
+        }
+    }
+
+    function mostrarToast(mensaje) {
+        const toastDiv = document.querySelector('#toastAgregar'); //* Seleccionamos el toast que esta en nuestro HTML
+        const toastBody = document.querySelector('#toast-body-agregar'); //* Y también el body para agregar contenido a nuestro toast
+        /// Creamos la instancia
+        const toast = new bootstrap.Toast(toastDiv);
+        toastBody.textContent = mensaje;
+        /// Mostrando el mensaje
+        toast.show();
+    }
+})();
