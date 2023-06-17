50b77afeb9ad0b8162c77693d29f5c6d1098e4c2
Ts2533 <tomasvanegas2533@gmail.com>
2023-06-16 21:13:20 -0500

    Modales

--- js/funcionalidadesRoles/validacionRolEditar.js
+++ js/funcionalidadesRoles/validacionRolEditar.js
@@ -36,29 +36,35 @@
 
         //- Expresiones Regulares
         const number = /^\D*$/;
-        const text = /^[^a-zA-Z]*$/;
-        const email_val =
-            /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-        //* Contenedores del formularios
-        const divNombre = document.querySelector(
-            '#formularioeditarrol div[name="divNombre"]'
-        );
-        
-        /// Lógica de validación
-
         let isValidado = true;
 
+        /// Lógica de validación
         //* Validaciones para el nombre
         if (nombre.value == '') {
-            imprimirAlerta('El nombre es obligatorio', divNombre, 'Nombre');
-            isValidado = false;
-        } else if (!number.test(nombre.value)) {
-            imprimirAlerta('El nombre no puede contener números', divNombre),
-                'Nombre';
-            isValidado = false;
-        }
 
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El nombre es obligatorio',
+                 })
+                   isValidado = false 
+        } else if (!number.test(nombre.value)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El nombre no puede contener números',
+                 })
+            isValidado = false; 
+            
+        }else if (!nombre.value.trimStart()){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'El nombre no puede ser un espacio',
+                     })
+                isValidado = false;  
+        }
+                
         if (isValidado) {
             //* Serrando el modal
             const modalBootstrap = bootstrap.Modal.getInstance(
@@ -68,43 +74,12 @@
 
             formulario.reset();
 
-            mostrarToast('Rol editado correctamente');
-        }
-    }
-
-    function imprimirAlerta(mensaje, lugar, clase) {
-        /// Verificar que no exista la alerta
-        const alert = document.querySelector(`.alerta${clase}`);
-
-        if (!alert) {
-            //? Crear alerta
-            const divMensaje = document.createElement('div');
-
-            divMensaje.classList.add(
-                // 'px-2',
-                'py-1',
-                'rounded',
-                'max-w-lg',
-                'mx-auto',
-                'mt-2',
-                'text-center',
-                'border',
-                `alerta${clase}`
-            );
-
-            divMensaje.classList.add(
-                'bg-red-100',
-                'border-red-400',
-                'text-red-700'
-            );
-
-            divMensaje.textContent = mensaje;
-
-            lugar.parentNode.insertBefore(divMensaje, lugar.nextSibling);
-
-            setTimeout(() => {
-                divMensaje.remove();
-            }, 4500);
+            mostrarToast(
+                Swal.fire(
+                    'Rol editado correctamente',
+                    '',
+                    'success'
+            ));
         }
     }
 
