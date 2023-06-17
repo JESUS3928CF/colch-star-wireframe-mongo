ee0e64421700bb784434756ce05d10a913cd26b4
Brian-3112 <pepito123@gmail.com>
2023-06-16 22:07:27 -0500

    modificacion

--- js/funcionalidadesProducto/validarProducto.js
+++ js/funcionalidadesProducto/validarProducto.js
@@ -14,7 +14,7 @@
         atras.addEventListener('click', recetearFormulario);
     });
 
-    function recetearFormulario(e){
+    function recetearFormulario(e) {
         e.preventDefault();
         formulario.reset();
     }
@@ -55,89 +55,89 @@
         const email_val =
             /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-        //* Contenedores del formularios
-        const divProducto = document.querySelector(
-            '#formularioAgregarProducto div[name="divProducto"]'
-        );
-
-        const divCantidad = document.querySelector(
-            '#formularioAgregarProducto div[name="divCantidad"]'
-        );
-
-        const divPrecio = document.querySelector(
-            '#formularioAgregarProducto div[name="divPrecio"]'
-        );
-
-        const divTalla = document.querySelector(
-            '#formularioAgregarProducto div[name="divTalla"]'
-        );
-
-        const divTela = document.querySelector(
-            '#formularioAgregarProducto div[name="divTela"]'
-        );
 
         /// Lógica de validación
 
         let isValidado = true;
 
-        //* Validaciones para el producto
-        if (producto.value == '') {
-            imprimirAlerta('Este campo es obligatorio', divProducto, 'Producto');
-            isValidado = false;
+        //* Validaciones para todos los campos
+        if (producto.value == '' && cantidad.value == "" && precio.value == "" && talla.value == "" && tela.value == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Todos los campos son obligatorios',
+            })
+            isValidado = false
+
+            //* Validaciones para producto
+        } else if (producto.value == '') {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El producto es obligatorio',
+            })
+            isValidado = false
         } else if (!number.test(producto.value)) {
-            imprimirAlerta('Este Campo no puede contener números', divProducto,
-                'Producto');
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El producto no puede contener números',
+            })
             isValidado = false;
+
+            //validacion de cantidad
+        } else if (cantidad.value == '') {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'la cantidad es obligatorio',
+            })
+            isValidado = false;
+
+        } else if (!nombre.value.trimStart()) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'la cantidad no puede ser un espacio',
+            })
+            isValidado = false;
+            //validacion de precio
+        }else if (precio.value == '') {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'el precio  es obligatorio',
+            })
+            isValidado = false;
+
+            //validacion de talla
+
+        } else if (talla.value == '') {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'la talla es obligatorio',
+            })
+            isValidado = false
+        }
+        //validacion tela
+        else if (tela.value == '') {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'la tela es obligatorio',
+            })
+            isValidado = false
         }
 
-        //* Validaciones para la cantidad
-        if (cantidad.value == '') {
-            imprimirAlerta(
-                'Este campo es obligatorio',
-                divCantidad,
-                'Cantidad'
-            );
-            isValidado = false;
-        } else if (!text.test(cantidad.value)) {
-            imprimirAlerta(
-                'Este campo no puede contener letras',
-                divCantidad,
-                'Cantidad'
-            );
-            isValidado = false;
-        }
-        //* Validaciones para el precio
-        if (precio.value == '') {
-            imprimirAlerta(
-                'Este campo es obligatorio',
-                divPrecio,
-                'Precio'
-            );
-            isValidado = false;
-        } else if (!text.test(precio.value)) {
-            imprimirAlerta(
-                'Este campo no puede contener letras',
-                divPrecio,
-                'Precio'
-            );
-            isValidado = false;
-        }
 
-        //* Validaciones para la talla
-        if (talla.value == '') {
-            imprimirAlerta('Este campo es obligatorio', divTalla, 'Talla');
-            isValidado = false;
-        } 
-        //* Validaciones para la Tela
-        if (tela.value == '') {
-            imprimirAlerta('Este campo es obligatorio', divTela, 'Tela');
-            isValidado = false;
-        } else if (!number.test(tela.value)) {
-            imprimirAlerta('Este Campo no puede contener números', divTela,
-                'Tela');
-            isValidado = false;
-        }
-        
+
+
 
         if (isValidado) {
             //* Serrando el modal
@@ -148,45 +148,17 @@
 
             formulario.reset();
 
-            mostrarToast('Producto agregado correctamente');
+            
+
+            mostrarToast(  Swal.fire(
+                'Usuario agregado correctamente',
+                '',
+                'success'
+              ));
         }
     }
 
-    function imprimirAlerta(mensaje, lugar, clase) {
-        /// Verificar que no exista la alerta
-        const alert = document.querySelector(`.alerta${clase}`);
 
-        if (!alert) {
-            //? Crear alerta
-            const divMensaje = document.createElement('div');
-
-            divMensaje.classList.add(
-                // 'px-2',
-                'py-1',
-                'rounded',
-                'max-w-lg',
-                'mx-auto',
-                'mt-2',
-                'text-center',
-                'border',
-                `alerta${clase}`
-            );
-
-            divMensaje.classList.add(
-                'bg-red-100',
-                'border-red-400',
-                'text-red-700'
-            );
-
-            divMensaje.textContent = mensaje;
-
-            lugar.parentNode.insertBefore(divMensaje, lugar.nextSibling);
-
-            setTimeout(() => {
-                divMensaje.remove();
-            }, 4500);
-        }
-    }
 
     function mostrarToast(mensaje) {
         const toastDiv = document.querySelector('#toastAgregar'); //* Seleccionamos el toast que esta en nuestro HTML
