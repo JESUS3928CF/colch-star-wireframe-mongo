cc8c7f1b6b6ccc26322986153fe796c85d03669d
Ts2533 <tomasvanegas2533@gmail.com>
2023-06-19 17:39:16 -0500

    Correccion de validacion de compras

--- js/funcionaidadesCompras/validarCompra.js
+++ js/funcionaidadesCompras/validarCompra.js
@@ -2,17 +2,24 @@
     const formulario = document.querySelector('#formularioagregarCompra');
 
 
-    const submit = document.querySelector(
-        '#compraG'
-    );
+    const submit = document.querySelector('#compraG');
+    const submitS=document.querySelector('#AgregarProductoS')
 
     const cancelar = document.querySelector('#guardarCancelado');
     const atras = document.querySelector('#xAgregar');
 
     window.addEventListener('load', () => {
-        submit.addEventListener('click', crearRol);
+        if(submitS){
+            submitS.addEventListener('click', crearProducto)
+            console.log('Producto')
+
+        if (submit){
+            submit.addEventListener('click',crearCompra)
+            console.log('Guaradar')
+        }
         cancelar.addEventListener('click', recetearFormulario);
         atras.addEventListener('click', recetearFormulario);
+    }
     });
 
     function recetearFormulario(e){
@@ -20,121 +27,131 @@
         formulario.reset();
     }
 
-    function crearRol(e) {
+    function crearCompra(e) {
         e.preventDefault();
 
         /// Validar el formulario
-        validarRol();
+        Compra();       
     }
 
-    function validarRol() {
-        //* Campos a validar
+  function  crearProducto(e){
+        e.preventDefault();
+        Producto();
+    }
 
-        const nombreP = document.querySelector(
-            '#nombreGuardar'
-        );
+function variables(){
+    const nombreP = document.querySelector('#nombreGuardar');
+    const nombreA = document.querySelector('#nombreCompraAgregar');
+    const precio = document.querySelector('#precioCompraAgregar');
+    const cantidad =document.querySelector('#cantidaCompraAgregar');
 
-        const nombreA = document.querySelector(
-            '#nombreCompraAgregar'
-        );
+    //- Expresiones Regulares
+    const number = /^\D*$/;
+    const text = /^[^a-zA-Z]*$/;
+    var signo = /[|°!"#$%&/()=?¿]/;
+    /// Lógica de validación
+    let isValidado = true;
 
-        const precio = document.querySelector(
-            '#precioCompraAgregar'
-        );
+    return{
+        nombreProveedor:nombreP,
+        nombreProducto: nombreA,
+        precio:precio,
+        cantidad:cantidad,
+        Numeros: number,
+        Texto:text,
+        Signos:signo,
+        Validar:isValidado
+    }
+} 
 
-        const cantidad =document.querySelector(
-            '#cantidaCompraAgregar'
-        );
-        
 
-        //- Expresiones Regulares
-        const number = /^\D*$/;
-        const text = /^[^a-zA-Z]*$/;
-        var signo = /[|°!"#$%&/()=?¿]/;
-        /// Lógica de validación
 
-        let isValidado = true;
 
-        if(nombreA.value=="" && nombreP.value =="" && precio.value=="" && cantidad.value==""){
+    function Compra() {
+
+        const compras=variables()
+
+        if(compras.nombreProducto.value=="" && compras.nombreProveedor.value=="" && compras.precio.value=="" && compras.cantidad.value==""){
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'Todos los campos son obligatorios',
                  })
-             isValidado = false;
+             compras.Validar=false
 
         //* Validaciones para el nombre proveedor
-        }else if (nombreP.value == '') {
+        }else if (compras.nombreProveedor.value == '') {
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre del proveedor es obligatorio',
                  })
-             isValidado = false;
+             compras.Validar=false
 
-        } else if (!number.test(nombreP.value)) {
+        } else if (!compras.Numeros.test(compras.nombreProveedor.value)) {
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre del proveedor no puede contener números',
                  })
-            isValidado = false;
-        }else if (!nombreP.value.trimStart()){
+            compras.Validar=false
+        }else if (!compras.nombreProveedor.value.trimStart()){
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre del proveedor no puede ser un espacio',
                  })
-            isValidado = false;  
-        }else if (signo.test(nombreP.value)) {
+            compras.Validar=false  
+        }else if (compras.Signos.test(compras.nombreProveedor.value)) {
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'No se puede poner signos en el nombre del proveedor',
                  })
-            isValidado = false;
+            
+            compras.Validar=false
 
            //*Validacion nombre producto 
-        }else if (nombreA.value == '') {
+        }else if (compras.nombreProducto.value == '') {
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre del producto es obligatorio',
                  })
-             isValidado = false;
+             compras.Validar=false
 
-        } else if (!number.test(nombreA.value)) {
+        } else if (!compras.Numeros.test(compras.nombreProducto.value)) {
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre del producto no puede contener números',
                  })
-            isValidado = false;
-        }else if (!nombreA.value.trimStart()){
+            compras.Validar=false
+        }else if (!compras.nombreProducto.value.trimStart()){
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre del producto no puede ser un espacio',
                  })
-            isValidado = false;  
-        }else if (signo.test(nombreA.value)) {
+            compras.Validar=false  
+        }else if (compras.Signos.test(compras.nombreProducto.value)) {
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'No se puede poner signos en el nombre del producto',
                  })
-            isValidado = false;
+            compras.Validar=false
 
-        } else if (cantidad.value == '') {
+        } else if (compras.cantidad.value == '') {
 
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'La cantidad es obligatorio',
             })
-            isValidado = false;
+            compras.Validar=false
 
-        }else if (!text.test(cantidad.value)){
+        }else if (!compras.Texto.test(compras.cantidad.value)){
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
@@ -142,7 +159,7 @@
             })
             isValidado=false
 
-        }else if (!cantidad.value.trimStart()){
+        }else if (!compras.cantidad.value.trimStart()){
             Swal.fire({
                 icon:'error',
                 title:'Error',
@@ -150,7 +167,7 @@
             });
             isValidado=false
 
-        }else if (signo.test(cantidad.value)){
+        }else if (compras.Signos.test(compras.cantidad.value)){
             Swal.fire({
                 icon:'error',
                 title:'Error',
@@ -160,44 +177,46 @@
 
         
             //validacion de precio
-        } else if (precio.value == '') {
+        } else if (compras.precio.value == '') {
 
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'El precio  es obligatorio',
             })
-            isValidado = false;
-        }else if (!text.test(precio.value)){
+            compras.Validar=false
+        }else if (!compras.Texto.test(compras.precio.value)){
             Swal.fire({
                 icon:'error',
                 title:'Error',
                 text: 'El precio no puede contener letras'
             })
-            isValidado=false
-        }else if (!precio.value.trimStart()){
+            compras.Validar=false
+
+
+
+        }else if (!compras.precio.value.trimStart()){
             Swal.fire({
                 icon:'error',
                 title:'Error',
                 text:'El precio no puede ser un campo vacio'
             });
-            isValidado=false
+            compras.Validar=false
 
 
-        }else if (signo.test(precio.value)){
+
+        }else if (compras.Signos.test(compras.precio.value)){
             Swal.fire({
                 icon:'error',
                 title: 'Error',
                 text: 'No se puede poner signos en el precio'
             })
+            compras.Validar=false
 
-            isValidado=false
+
         }
 
-
-
-
-        if (isValidado) {
+        if (compras.Validar) {
             //* Serrando el modal
             const modalBootstrap = bootstrap.Modal.getInstance(
                 document.querySelector('#myModal')
@@ -206,22 +225,141 @@
 
             formulario.reset();
 
-            mostrarToast( Swal.fire(
+            Swal.fire(
                 'Compra agregada correctamente',
                 '',
                 'success'
-              ))
+            )
         }
     }
 
 
-    function mostrarToast(mensaje) {
-        const toastDiv = document.querySelector('#toastAgregar'); //* Seleccionamos el toast que esta en nuestro HTML
-        const toastBody = document.querySelector('#toast-body-agregar'); //* Y también el body para agregar contenido a nuestro toast
-        /// Creamos la instancia
-        const toast = new bootstrap.Toast(toastDiv);
-        toastBody.textContent = mensaje;
-        /// Mostrando el mensaje
-        toast.show();
+ 
+
+
+    function Producto(){
+        const productos=variables()
+
+        if(productos.nombreProducto.value=="" && productos.precio.value=="" && productos.cantidad.value==""){
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Todos los campos son obligatorios',
+                 })
+             productos.Validar=false
+
+           //*Validacion nombre producto 
+        }else if (productos.nombreProducto.value == '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El nombre del producto es obligatorio',
+                 })
+             productos.Validar=false
+
+        } else if (!productos.Numeros.test(productos.nombreProducto.value)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El nombre del producto no puede contener números',
+                 })
+            productos.Validar=false
+
+        }else if (!productos.nombreProducto.value.trimStart()){
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El nombre del producto no puede ser un espacio',
+                 })
+            productos.Validar=false  
+        }else if (productos.Signos.test(productos.nombreProducto.value)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'No se puede poner signos en el nombre del producto',
+                 })
+            productos.Validar=false
+
+        } else if (productos.cantidad.value == '') {
+            
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'La cantidad es obligatorio',
+            })
+            productos.Validar=false
+
+        }else if (!productos.Texto.test(productos.cantidad.value)){
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'La cantidad no pueden contener letras'
+            })
+            productos.Validar=false
+
+        }else if (!productos.cantidad.value.trimStart()){
+            Swal.fire({
+                icon:'error',
+                title:'Error',
+                text:'La cantidad no puede ser un campo vacio'
+            });
+            productos.Validar=false
+
+        }else if (productos.Signos.test(productos.cantidad.value)){
+            Swal.fire({
+                icon:'error',
+                title:'Error',
+                text: 'No se puede poner signos en la cantidad'
+            })
+            productos.Validar=false
+
+        
+            //validacion de precio
+        } else if (productos.precio.value == '') {
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'El precio  es obligatorio',
+            })
+            productos.Validar=false
+        }else if (!productos.Texto.test(productos.precio.value)){
+            Swal.fire({
+                icon:'error',
+                title:'Error',
+                text: 'El precio no puede contener letras'
+            })
+
+            productos.Validar=false
+
+        }else if (!productos.precio.value.trimStart()){
+            Swal.fire({
+                icon:'error',
+                title:'Error',
+                text:'El precio no puede ser un campo vacio'
+            });
+            productos.Validar=false
+
+
+        }else if (productos.Signos.test(productos.precio.value)){
+            Swal.fire({
+                icon:'error',
+                title: 'Error',
+                text: 'No se puede poner signos en el precio'
+            })
+
+            productos.Validar=false
+        }
+
+        if (productos.Validar) {
+            //* Serrando el modal
+            formulario.reset();
+            Swal.fire(
+                'Producto agregada correctamente',
+                '',
+                'success'
+            )
+        }
     }
+
 })();
